a02489caea6f13f64ae90925c3c9643e10dde5e2
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-04-13 23:18:17 +0100

    implement template iter function to apply function on array elements

---
+++ cpp07_templates/ex01/iter.hpp
@@ -0,0 +1,18 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template <typename T> void iter(T *array, int size, void (*func)(T&)) {
+    for (int i = 0; i < size; i++) {
+        func(array[i]);
+    }
+}
+
+template <typename T> void iter(T *array, const int size, void (*func)(const T&)) {
+    for (int i = 0; i < size; i++) {
+        func(array[i]);
+    }
+}
+
+#endif
---
+++ cpp07_templates/ex01/main.cpp
@@ -0,0 +1,13 @@
+#include "iter.hpp"
+
+template <typename T>
+void print(const T& x) {
+    std::cout << x << std::endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    iter(arr, sizeof(arr) / sizeof(arr[0]), print);
+    std::string str[] = {"one", "two", "three"};
+    iter(str, sizeof(str) / sizeof(str[0]), print);
+}
