3c2890d04b740173d1454e239fbcc2d9b2933c89
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-07-21 22:28:11 +0100

    refactor: add all funcs to pmergeme file to make main file clear

--- cpp09_algorithms/ex02/PmergeMe.hpp
+++ cpp09_algorithms/ex02/PmergeMe.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cctype>
+#include <utility>
 #include <vector>
 #include <iostream>
 #include <cstdlib>
@@ -18,6 +19,7 @@ class PmergeMe {
                     int &odd);
         void recursiveSort(std::vector<int> &chain);
         void Sort();
-        void insertElements(std::vector<int> &main, std::vector<int> &pending);
+        void insertElements(std::vector<int> &main, std::vector<std::pair<int, int> > &pairs);
         int binarysearch(std::vector<int> &mainChain, int value);
+        int binarysearch(std::vector<int> &mainChain, int value, int limit);
 };
